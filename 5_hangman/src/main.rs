f5b6d587e6f29ac66859957468cb03afe9fc4634
Thomas Grem <thomas.grem@squer.io>
2024-06-20 14:04:36 +0200

    Began solution

--- 5_hangman/src/main.rs
+++ 5_hangman/src/main.rs
@@ -1,3 +1,20 @@
+use std::error::Error;
+
 fn main() {
-    println!("Hello, world!");
+    let word = match get_word() {
+        Ok(w) => w,
+        Err(_) => {
+            println!("Failed to get word");
+            return;
+        }
+    };
+    println!("Random word: {}", word);
+}
+
+fn get_word() -> Result<String, Box<dyn Error>> {
+    let resp = reqwest::blocking::get("https://random-word-api.herokuapp.com/word?lang=en")?;
+
+    let mut word: Vec<String> = resp.json()?;
+
+    word.pop().ok_or(Box::from("No word found"))
 }
